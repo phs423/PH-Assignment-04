1617fae3d7b818e983c86c5bd6e39c673f4f4812
phs423 <phshawon22@gmail.com>
2026-02-24 16:19:04 +0600

    added 'rejected section and rejected button functionality

--- scripts/logic.js
+++ scripts/logic.js
@@ -18,27 +18,24 @@ document.getElementById('reject-btn').addEventListener('click', function () {
 document.getElementById('inter-btn').addEventListener('click', function () {
     document.getElementById('allCards').classList.add('hidden');
     document.getElementById('interviewSection').classList.remove('hidden');
-    document.getElementById('').classList.add('hidden');
+    document.getElementById('rejectSection').classList.add('hidden');
 })
 document.getElementById('all-btn').addEventListener('click', function () {
     document.getElementById('allCards').classList.remove('hidden');
     document.getElementById('interviewSection').classList.add('hidden');
-    document.getElementById('').classList.add('hidden');
+    document.getElementById('rejectSection').classList.add('hidden');
 })
-
-
-// document.getElementById('dlt').addEventListener('mouseover', function () {
-//     document.getElementById('dlt').classList.add('btn-cl');
-// })
-// document.getElementById('dlt').addEventListener('mouseout', function () {
-//     document.getElementById('dlt').classList.remove('btn-cl');
-// })
-
+document.getElementById('reject-btn').addEventListener('click', function () {
+    document.getElementById('allCards').classList.add('hidden');
+    document.getElementById('interviewSection').classList.add('hidden');
+    document.getElementById('rejectSection').classList.remove('hidden');
+})
 
 
 
 
 const interviewSection = document.getElementById('interviewSection');
+const rejectSection = document.getElementById('rejectSection');
 let interviewList = [];
 let rejectList = [];
 
@@ -81,7 +78,33 @@ mainContainer.addEventListener('click', function (event) {
         if (!companyExist) {
             interviewList.push(cardInfo)
         }
+        calculateCount();
         renderInterview();
+    }
+    else if (event.target.classList.contains('rejectButton')) {
+        const parentNode = event.target.parentNode.parentNode;
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const jobCat = parentNode.querySelector('.jobCat').innerText;
+        const jobType = parentNode.querySelector('.jobType').innerText;
+        const statuss = parentNode.querySelector('.statuss').innerText;
+        const descrpt = parentNode.querySelector('.descrpt').innerText;
+        
+        parentNode.querySelector('.statuss').innerText = 'REJECTED';
+        
+        const cardInfo = {
+            companyName,
+            jobCat,
+            jobType,
+            statuss: 'REJECTED',
+            descrpt
+        }
+        const companyExist = rejectList.find(item => item.companyName == cardInfo.companyName)
+        
+        if (!companyExist) {
+            rejectList.push(cardInfo)
+        }
+        calculateCount();
+        renderRejected();
     }
 
 })
@@ -108,6 +131,30 @@ function renderInterview() {
         `;
 
         interviewSection.appendChild(div);
+    }
+}
+function renderRejected() {
+    rejectSection.innerHTML = '';
+    for (let reject of rejectList) {
+        console.log(reject);
+        let div = document.createElement('div');
+        div.className = 'bg-white p-6 rounded-xl mt-6';
+        div.innerHTML = `
+            <div class="flex justify-between items-center">
+                    <h2 class="companyName font-bold text-xl">${reject.companyName}</h2>
+                    <button id="dlt" class="dlt rounded-full bg-blue-200 p-2"><i class="fa-regular fa-trash-can"></i></button>
+                </div>
+                <p class="jobCat text-[#64748B]">${reject.jobCat}</p>
+                <p class="jobType text-[#64748B]">${reject.jobType}</p>
+                <p class="statuss bg-blue-200 px-4 py-2 rounded font-semibold max-w-fit">${reject.statuss}</p>
+                <p class="descrpt">${reject.descrpt}</p>
+                <div class="flex gap-2 flex-wrap">
+                    <button class="border-2 border-green-600 px-4 py-2 rounded font-semibold text-green-600">INTERVIEW</button>
+                    <button class="border-2 border-red-600 px-4 py-2 rounded font-semibold text-red-600">REJECTED</button>
+                </div>
+        `;
+
+        rejectSection.appendChild(div);
     }
 }
 
