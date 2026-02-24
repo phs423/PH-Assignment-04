4952e06677aa2beddfd47e53c823405bc5c1926c
phs423 <phshawon22@gmail.com>
2026-02-24 07:51:16 +0600

    added count functionality

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,24 +1,38 @@
-document.getElementById('all-btn').addEventListener('click', function(){
+document.getElementById('all-btn').addEventListener('click', function () {
     document.getElementById('all-btn').classList.add('btn-cl');
     document.getElementById('inter-btn').classList.remove('btn-cl');
     document.getElementById('reject-btn').classList.remove('btn-cl');
 })
-document.getElementById('inter-btn').addEventListener('click', function(){
+document.getElementById('inter-btn').addEventListener('click', function () {
     document.getElementById('all-btn').classList.remove('btn-cl');
     document.getElementById('inter-btn').classList.add('btn-cl');
     document.getElementById('reject-btn').classList.remove('btn-cl');
 })
-document.getElementById('reject-btn').addEventListener('click', function(){
+document.getElementById('reject-btn').addEventListener('click', function () {
     document.getElementById('all-btn').classList.remove('btn-cl');
     document.getElementById('inter-btn').classList.remove('btn-cl');
     document.getElementById('reject-btn').classList.add('btn-cl');
 })
-document.getElementById('dlt').addEventListener('mouseover', function(){
+document.getElementById('dlt').addEventListener('mouseover', function () {
     document.getElementById('dlt').classList.add('btn-cl');
 })
-document.getElementById('dlt').addEventListener('mouseout', function(){
+document.getElementById('dlt').addEventListener('mouseout', function () {
     document.getElementById('dlt').classList.remove('btn-cl');
 })
 
+
+let total = document.getElementById('total');
+let interCount = document.getElementById('interCount');
+let rejectCount = document.getElementById('rejectCount');
+const allCardSection = document.getElementById('allCards')
+function calculateCount() {
+    total.innerText = allCardSection.children.length;
+    interCount.innerText = interviewList.length;
+    rejectCount.innerText = rejectList.length;
+}
+calculateCount()
+
+let interviewList = [];
+let rejectList = [];
 
 
